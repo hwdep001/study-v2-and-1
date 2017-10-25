bf8fa80b49db7f0c2027ae875a9e97d728907dab
howoh <hwdep001@gmail.com>
2017-10-25 21:11:37 +0900

    Initial commit

---
+++ app/src/main/java/com/howoh/studyv2/studyv2_1/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.howoh.studyv2.studyv2_1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        findViewById(R.id.btn_sign_in).setOnClickListener(signInBtnClickListener);
+    }
+
+    Button.OnClickListener signInBtnClickListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
+    };
+}
