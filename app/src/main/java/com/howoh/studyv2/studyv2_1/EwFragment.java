83228a9191a864a4088f6394d5ca08a0a4363356
howoh <hwdep001@gmail.com>
2017-10-26 17:53:07 +0900

    update info layout

--- app/src/main/java/com/howoh/studyv2/studyv2_1/EwFragment.java
+++ app/src/main/java/com/howoh/studyv2/studyv2_1/EwFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * Created by howoh on 2017-10-26.
@@ -13,9 +15,16 @@ import android.view.ViewGroup;
 
 public class EwFragment extends Fragment {
 
+    static final String[] LIST_MENU = {"LIST1", "LIST2", "LIST3"} ;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.ew_fragment, container, false);
+
+//        ArrayAdapter ad = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, LIST_MENU);
+//        ListView lv = (ListView) v.findViewById(R.id.info_content1_list);
+//        lv.setAdapter(ad);
+
         return v;
     }
 }
--- app/src/main/java/com/howoh/studyv2/studyv2_1/InfoFragment.java
+++ app/src/main/java/com/howoh/studyv2/studyv2_1/InfoFragment.java
@@ -1,10 +1,13 @@
 package com.howoh.studyv2.studyv2_1;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * Created by howoh on 2017-10-26.
@@ -17,4 +20,5 @@ public class InfoFragment extends Fragment {
         View v = inflater.inflate(R.layout.info_fragment, container, false);
         return v;
     }
+
 }
